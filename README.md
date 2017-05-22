e5a65e1322b0475c16bd9ac78205af43c67bcf10
heyan <heyan@xin.com>
2017-05-22 10:59:13 +0800

    modifydbsite

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # hyblog
 hyblog
 贺燕博客
+本地访问: localhost:3000
+线上访问: https://hyblog1.herokuapp.com/
+欢迎补充
--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports={
     secret:'zfpx',
-    url:'mongodb://123.57.143.189/hyblog'
+    url:'mongodb://127.0.0.1:27017/zfpx'
     /*这是阿里巴巴的服务器，项目中用的数据库必须是在服务器上的，如果用本地的别人是无法访问到的*/
 }
 
